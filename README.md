e19ac5a952634c07d3232b761faeb293b90d3456
MantasMikal <mantas.codes@gmail.com>
2021-05-27 16:03:17 +0100

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pluto
+Dark VS Code theme heavily inspired by [Horizon](https://marketplace.visualstudio.com/items?itemName=jolaleye.horizon-theme-vscode) and [One Dark Pro](https://marketplace.visualstudio.com/items?itemName=zhuangtongfa.Material-theme)
---
+++ src/dark/build.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const mustache = require('mustache')
+
+const globals = require('./globals.json')
+const template = JSON.stringify(require(`${__dirname}/template.json`))
+
+const content = require(`${__dirname}/pluto.json`)
+const theme = mustache.render(template, { ...content, ...globals })
+fs.writeFileSync(`${__dirname}/../../themes/pluto.json`, theme)
+
